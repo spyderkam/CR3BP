f423770dd7ae1564652069e627f81b14350338ec
Kam <58607149+spyderkam@users.noreply.github.com>
2024-09-17 19:33:51 -0500

    Update rk4step.f95

--- rk4step.f95
+++ rk4step.f95
@@ -23,17 +23,14 @@ subroutine rk4step ( x, h, y )
       real(dp), intent(in), dimension(n_eq) :: y
       real(dp), dimension(n_eq) :: f, k
       real(dp) :: r1, r2
-          !---------------------
-      r1 = sqrt((y(1)-(1-mu))**2 + y(2)**2) !distance vector with Earth
-  		r2 = sqrt((y(1)+mu)**2 + y(2)**2) !distance vector with Sun
+          !---------------------------------
+      r1 = sqrt((y(1)-(1-mu))**2 + y(2)**2)    ! distance vector with Earth
+      r2 = sqrt((y(1)+mu)**2 + y(2)**2)        ! distance vector with Sun
 
-			f(1:2) = y(3:4)
-
-      f(3) = -(mu*(y(1)-1+mu))/r1**3 - ((1-mu)*(mu+y(1)))/r2**3 &
-					   + y(1) + 2*y(4)
-
-			f(4) = - (mu*y(2))/r1**3 - ((1-mu)*y(2))/r2**3 + y(2) - 2*y(3)
-            !----------------
+      f(1:2) = y(3:4)
+      f(3) = -(mu*(y(1)-1+mu))/r1**3 - ((1-mu)*(mu+y(1)))/r2**3 + y(1) + 2*y(4)
+      f(4) = - (mu*y(2))/r1**3 - ((1-mu)*y(2))/r2**3 + y(2) - 2*y(3)
+          !---------------------------------
       k = dt * f
     end function kv
 end subroutine rk4step
